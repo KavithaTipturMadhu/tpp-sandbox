plaidml/tpp-mlir

Tpp pack to identity pass

lib/TPP/DefaultTppPasses.cpp
@@ -261,6 +261,7 @@ private:
     pm.addPass(createCleanupPass());
     pm.addPass(createTileConsumerAndFuseProducersPass());
     pm.addPass(createCleanupPass());
+    pm.addPass(createConvertTppToIdentityPass());
 
     // Generalize tensor.pack and tensor.unpack.
     pm.addPass(createGeneralizeTensorPackAndUnPackPass());

KavithaTipturMadhu/tpp-sandbox

lib/TPP/DefaultTppPasses.cpp
@@ -261,6 +261,7 @@ private:
     pm.addPass(createCleanupPass());
     pm.addPass(createTileConsumerAndFuseProducersPass());
     pm.addPass(createCleanupPass());
+    pm.addPass(createConvertTppPackUnpack());
 
     // Generalize tensor.pack and tensor.unpack.
     pm.addPass(createGeneralizeTensorPackAndUnPackPass());